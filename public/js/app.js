4602f6d3509ff20b7a51035ac7ca728f15cb2a51
Qian Kai <qiankai@ip-172-31-21-140.us-west-2.compute.internal>
2026-03-02 15:58:27 +0800

    fix(ui): add missing escapeHtml/escapeAttr definitions causing meeting detail to fail

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,21 @@
+/* ===== Utility: HTML escaping ===== */
+function escapeHtml(str) {
+  if (str == null) return "";
+  return String(str)
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
+function escapeAttr(str) {
+  if (str == null) return "";
+  return String(str)
+    .replace(/&/g, "&amp;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
+
 /* ===== Random Fun Messages ===== */
 const FunMessages = {
   uploading: [
