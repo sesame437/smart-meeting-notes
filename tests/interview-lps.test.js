8c0332b42e4c51664f6ad2c66de1b9f504911f10
sesame437 <sesame.qian@gmail.com>
2026-05-12 17:15:15 +0800

    feat(interview-lps): add shared 9-LP whitelist for interview subtypes

---
+++ tests/interview-lps.test.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const { INTERVIEW_LPS, isValidLP } = require("../services/interview-lps");
+
+describe("INTERVIEW_LPS whitelist", () => {
+  test("contains exactly the 9 expected LPs in order", () => {
+    expect(INTERVIEW_LPS).toEqual([
+      "Learn and Be Curious",
+      "Ownership",
+      "Customer Obsession",
+      "Dive Deep",
+      "Have Backbone; Disagree and Commit",
+      "Invent and Simplify",
+      "Deliver Results",
+      "Earn Trust",
+      "Think Big",
+    ]);
+  });
+
+  test("whitelist is frozen and cannot be mutated", () => {
+    expect(Object.isFrozen(INTERVIEW_LPS)).toBe(true);
+    expect(() => {
+      INTERVIEW_LPS.push("New LP");
+    }).toThrow();
+  });
+
+  test("isValidLP returns true for whitelisted names", () => {
+    expect(isValidLP("Ownership")).toBe(true);
+    expect(isValidLP("Have Backbone; Disagree and Commit")).toBe(true);
+  });
+
+  test("isValidLP returns false for non-whitelisted names", () => {
+    expect(isValidLP("Bias for Action")).toBe(false);
+    expect(isValidLP("ownership")).toBe(false);
+    expect(isValidLP("")).toBe(false);
+    expect(isValidLP(null)).toBe(false);
+    expect(isValidLP(undefined)).toBe(false);
+  });
+});
